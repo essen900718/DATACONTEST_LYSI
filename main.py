e27ef24e8a16353318ed78531180fe461c11be67
essen900718 <essen900718@gmail.com>
2022-06-03 22:44:21 +0800

    6

--- main.py
+++ main.py
@@ -1,15 +1,7 @@
 from flask import Flask, render_template
-import pandas
 
 app = Flask(__name__)
 
-
-@app.route('/joball')
-def joball():
-    filename = 'job_test.csv'
-    data = pandas.read_csv(filename, header=0)
-    myData = data.values
-    return render_template('home.html', myData=myData)
 
 @app.route("/")
 def index():
@@ -31,22 +23,8 @@ def type():
 def location():
     return render_template("location.html")
 
-@app.route("/job1")
-def job1():
-    filename = 'job_test.csv'
-    data = pandas.read_csv(filename, header=0)
-    myData = data.values
-    return render_template("job1.html")
 
-@app.route("/jobone/<index>")
-def jobone(index):
-    print(index)
-    id = int(index)
-    filename = 'job_test.csv'
-    data = pandas.read_csv(filename, header=0)
-    myData = data.values[id]
-    return render_template("job_test.html", myData=myData)
-    
+ 
 
 @app.route("/job2")
 def job2():
