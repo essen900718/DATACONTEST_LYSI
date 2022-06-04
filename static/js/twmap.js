dde10359dfd56e9aeeec75d619472e21d5e05945
larry3425527 <larrye59678991s@gmail.com>
2022-06-04 18:18:24 +0800

    click

--- static/js/twmap.js
+++ static/js/twmap.js
@@ -2,18 +2,27 @@ $(function(){
   $("#none").click(function(){
     location.reload();
   });
-	$(".color").click(function(){
-		$(this).addClass('now');
-		$(".color").not(this).removeClass('now');
-		color = $('.now').attr('style').substr(12,7);
-		console.log(color);
-	});
-
+	// $(".color").click(function (){
+	// 	$(this).addClass('now');
+	// 	$(".color").not(this).removeClass('now');
+	// 	color = $('.now').attr('style').substr(12,7);
+	// 	console.log(color);
+	// });
 	color = $('.now').attr('style').substr(12,7);
 	console.log(color);
+	let clicked = false;
 	$(".twt-map-path").click(
 		function(){
+			if (clicked){
+				clicked = false
+				color = "#e3b92c"
+			}else{
+				clicked = true
+				color = $('.now').attr('style').substr(12,7);
+			}
 			$(this).css('fill',color);
-	});
+			console.log(clicked)
+		}
+	);
 
 });
